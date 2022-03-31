90479d5d6520c64d3f1dfcbc407e145e7db2356c
knm22mukk <knm22mukk@gmail.com>
2022-03-31 23:08:42 +0900

    画像、基礎データの一部設定、_document.tsxの設定

---
+++ src/data/siteMetaData.ts
@@ -0,0 +1,9 @@
+export const siteMetaData = {
+  title: '株式会社橋市',
+  description:
+    '橋市では包装資材・厨房機器類・食器・梱包資材・事務用品等々の幅広い商品を取り扱っている会社です。',
+  siteUrl: '',
+  siteLogo: '/images/logo240x240.jpg',
+  email: '',
+  twitter: 'https://twitter.com/hashiichi_841',
+};
---
+++ src/pages/_document.tsx
@@ -0,0 +1,41 @@
+import type { DocumentContext, DocumentInitialProps } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { siteMetaData } from '@data/siteMetaData';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return initialProps;
+  }
+  render(): JSX.Element {
+    return (
+      <Html lang='ja-JP'>
+        <Head>
+          <meta name='application-name' content={siteMetaData.title} />
+          <meta name='apple-mobile-web-app-capable' content='yes' />
+          <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+          <meta name='apple-mobile-web-app-title' content={siteMetaData.title} />
+          <meta name='format-detection' content='telephone=no' />
+          <meta name='mobile-web-app-capable' content='yes' />
+          <meta name='theme-color' content='#3b82f6' />
+
+          <link rel='apple-touch-icon' sizes='180x180' href='/favicons/apple-touch-icon.png' />
+          <link rel='icon' type='image/png' sizes='32x32' href='/favicons/favicon-32x32.png' />
+          <link rel='icon' type='image/png' sizes='16x16' href='/favicons/favicon-16x16.png' />
+          <link rel='manifest' href='/favicons/.webmanifest' />
+          <link rel='mask-icon' href='/favicons/safari-pinned-tab.svg' color='#000000' />
+          <link rel='shortcut icon' href='/favicons/favicon.ico' />
+          <meta name='msapplication-TileColor' content='#ffffff' />
+          <meta name='msapplication-config' content='/favicons/browserconfig.xml' />
+          <meta name='theme-color' content='#ffffff' />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
