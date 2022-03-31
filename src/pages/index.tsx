c56e031ca9166cc6db6751e054ebba7c11d84c17
knm22mukk <knm22mukk@gmail.com>
2022-03-31 23:11:30 +0900

    react-iconsの導入

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next';
+import { AiFillHome } from 'react-icons/ai';
 
 const Home: NextPage = () => {
   return (
     <div>
       <h1 className='text-red-900'>ホームです。</h1>
+      <AiFillHome />
     </div>
   );
 };
