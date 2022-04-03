70c262b0b9badf5290abe9e6042160982ec408f6
knm22mukk <knm22mukk@gmail.com>
2022-04-03 15:17:13 +0900

    求人ページの作成

--- src/pages/recruit/index.tsx
+++ src/pages/recruit/index.tsx
@@ -1,9 +1,22 @@
+import Image from 'next/image';
 import Layout from '@components/Layout';
+import PageHeader from '@components/PageHeader';
 
 const Recruit = () => {
   return (
     <Layout>
-      <h4 className=''>求人ページ</h4>
+      <PageHeader title='求人情報' />
+      <div className='py-12 text-center'>
+        <Image
+          src='/images/recruit.jpg'
+          width={800}
+          height={700}
+          alt='recruit-image'
+          className=''
+        />
+        <h4 className='text-2xl font-bold text-center'>現在正社員ドライバーを募集しています。</h4>
+        <p className='font-bold'>詳細・ご応募の際はハローワークを通してお願いいたします。</p>
+      </div>
     </Layout>
   );
 };
